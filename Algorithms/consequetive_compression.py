e39adbf98481fc49bc4e6fa318b44237c61b2534
messophet <danyyounan@gmail.com>
2015-09-20 22:57:24 -0400

    adding preliminary solution to consecutive numbers compression problem and simple queue

---
+++ Algorithms/consequetive_compression.py
@@ -0,0 +1,25 @@
+#terribly inefficient solution
+
+#check backward
+def print_consecutive(items):
+	temp = items[0]
+	outstring = []
+	for i in range(1,len(items)):
+		#if the current number is not consecutive of the previous
+		if(items[i]!=items[i-1]+1):
+			if(items[i-1]!=temp):
+				if(outstring!=[]):
+					outstring.append(',')
+				outstring.append(str(temp) + '-' + str(items[i-1]))
+			else:
+				outstring.append(','+str(temp))
+			temp = items[i]
+	if(items[-1]!=items[-2]+1):
+		outstring.append(',' + str(items[-1]))
+	else:
+		outstring.append(',' + str(temp) + '-' + str(items[-1]))		
+
+	return ''.join(outstring)
+
+print(print_consecutive([1,2,3,5,6,7,8,9,11,15,24,25]))
+
