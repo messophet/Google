6477e25df6cc9c8fd8a9936e5281956b11e7c05a
messophet <danyyounan@gmail.com>
2015-09-22 21:25:32 -0400

    Update queue.py

--- Algorithms/queue.py
+++ Algorithms/queue.py
@@ -4,7 +4,7 @@ class Queue:
 	def isEmpty(self):
 		return self.items == []
 	def enqueue(self, item):
-		self.items.insert(0,item)
+		self.items.insert(0,item) #insert at the front of the list (creates FIFO structure)
 	def dequeue(self):
 		return self.items.pop()
 	def size(self):
